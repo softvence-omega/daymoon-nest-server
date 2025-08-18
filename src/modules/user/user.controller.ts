462ff5c71c3c8ad7c80d574c3bad5f67981def96
sakibatreus <sakibatreus@gmail.com>
2025-08-18 14:20:04 +0600

    API updated

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { Body, Controller, Get, Patch, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserService } from './user.service';
 import { AuthUser } from '../auth/jwt.strategy';
@@ -12,18 +19,36 @@ import { Role } from './schemas/user.schema';
 export class UserController {
   constructor(private userService: UserService) {}
 
+  // @Get('profile')
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Roles('Admin', 'Buyer', 'Seller')
+  // async getProfile(@Request() req: { user: AuthUser }) {
+  //   const user = await this.userService.findById(req.user.userId);
+  //   return user;
+  // }
   @Get('profile')
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('Admin', 'Buyer', 'Seller')
   async getProfile(@Request() req: { user: AuthUser }) {
-    const user = await this.userService.findById(req.user.userId);
+  try {
+    const user = await this.userService.findByIdWithRelations(req.user.userId);
+    if (!user) {
+      return { message: 'User not found' };
+    }
     return user;
+  } catch (error) {
+    console.error('Error in getProfile():', error);
+    throw error;
   }
+}
 
   // Update profile (logged-in user)
   @UseGuards(JwtAuthGuard)
   @Patch('update-profile')
-  async updateProfile(@Request() req, @Body() body: Partial<{ fullName: string; email: string; mobile: string }>) {
+  async updateProfile(
+    @Request() req,
+    @Body() body: Partial<{ fullName: string; email: string; mobile: string }>,
+  ) {
     return this.userService.updateProfile(req.user.userId, body);
   }
 
